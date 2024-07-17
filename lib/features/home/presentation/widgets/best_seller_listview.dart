ed862c3d02e34e6edaf1654e61defcd21a7e4f52
ARasheed21 <ara4eed21@gmail.com>
2024-07-18 00:49:20 +0300

    create best seller listview

---
+++ lib/features/home/presentation/widgets/best_seller_listview.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:udemy8_bookly/features/home/presentation/widgets/book_listview_item.dart';
+
+class BestSellerListView extends StatelessWidget {
+  const BestSellerListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+      padding: EdgeInsets.zero,
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: BookListViewItem(),
+        );
+      },
+    );
+  }
+}
--- lib/features/home/presentation/widgets/home_screen_body.dart
+++ lib/features/home/presentation/widgets/home_screen_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:udemy8_bookly/features/home/presentation/widgets/best_seller_listview.dart';
 import 'package:udemy8_bookly/features/home/presentation/widgets/featured_books_listview.dart';
 import '../../../../core/utils/styles.dart';
 import 'book_listview_item.dart';
@@ -11,21 +12,31 @@ class HomeScreenBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Column(
-        children: const[
-          CustomAppBar(),
-          FeaturedBooksListView(),
-          SizedBox(
-            height: 50,
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const[
+                CustomAppBar(),
+                FeaturedBooksListView(),
+                SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  'Best Seller',
+                  style: Styles.textStyle18,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                BookListViewItem(),
+              ],
+            ),
           ),
-          Text(
-            'Best Seller',
-            style: Styles.textStyle18,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          BookListViewItem(),
+          SliverFillRemaining(
+            child: BestSellerListView(),
+          )
         ],
       ),
     );
