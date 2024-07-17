ed8a19ce1711096cd7b4268716dd9670605a7dd9
ARasheed21 <ara4eed21@gmail.com>
2024-07-17 22:39:45 +0300

    navigate to home screen

--- lib/features/splash/presentation/widgets/splash_screen_body.dart
+++ lib/features/splash/presentation/widgets/splash_screen_body.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/cupertino.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 import 'package:udemy8_bookly/core/utils/assets_paths.dart';
 
-class SplashScreenBody extends StatelessWidget {
+import '../../../../constants.dart';
+import '../../../home/presentation/screens/home_screen.dart';
+
+class SplashScreenBody extends StatefulWidget {
   const SplashScreenBody({super.key});
+
+  @override
+  State<SplashScreenBody> createState() => _SplashScreenBodyState();
+}
+
+class _SplashScreenBodyState extends State<SplashScreenBody> {
+
+  @override
+  void initState() {
+    navigateToHome();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +28,19 @@ class SplashScreenBody extends StatelessWidget {
       children: [
         Image.asset(AssetPath.logo),
       ],
+    );
+  }
+
+  void navigateToHome() {
+    Future.delayed(
+      const Duration(seconds: 2),
+          () {
+        Get.to(() => const HomeScreen(),
+            transition: Transition.fade,
+            duration: kTranstionDuration);
+
+        //GoRouter.of(context).push(AppRouter.kHomeView);
+      },
     );
   }
 }
