43fe21d5d24b92562d99e768ad1575dba5c4fa67
ARasheed21 <ara4eed21@gmail.com>
2024-07-18 15:25:46 +0300

    add book image to book details screen

--- lib/features/home/presentation/widgets/book_details_screen_body.dart
+++ lib/features/home/presentation/widgets/book_details_screen_body.dart
@@ -1,30 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-
 import 'custom_book_details_app_bar.dart';
+import 'custome_book_image.dart';
 
 class BookDetailsScreenBody extends StatelessWidget {
   const BookDetailsScreenBody({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
     return CustomScrollView(
       slivers: [
         SliverFillRemaining(
           hasScrollBody: false,
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            padding: const EdgeInsets.all(20),
             child: Column(
-              children: const [
-                CustomBookDetailsAppBar(),
-                Expanded(
-                  child: SizedBox(
-                    height: 50,
-                  ),
-                ),
-                SizedBox(
-                  height: 40,
-                ),
+              children:  [
+                const CustomBookDetailsAppBar(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: width*0.2),
+                  child:  const CustomBookImage(),
+                )
               ],
             ),
           ),
