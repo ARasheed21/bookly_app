f9aaa236d7270529427fcc3455a5fcde4a19e424
ARasheed21 <ara4eed21@gmail.com>
2024-07-19 18:43:12 +0300

    add home remote fetchFeaturedBooks method

--- lib/features/home/data/data_sources/home_remote_data_source.dart
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -1,5 +1,7 @@
+import 'package:udemy8_bookly/core/utils/api_service.dart';
 
 import '../../domain/entities/book_entity.dart';
+import '../models/book_model/book_model/book_model.dart';
 
 abstract class HomeRemoteDataSource {
   Future<List<BookEntity>> fetchFeaturedBooks();
@@ -7,10 +9,15 @@ abstract class HomeRemoteDataSource {
 }
 
 class HomeRemoteDataSourceImpl extends HomeRemoteDataSource{
+
+  final ApiService apiService;
+  HomeRemoteDataSourceImpl(this.apiService);
+
   @override
-  Future<List<BookEntity>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<List<BookEntity>> fetchFeaturedBooks() async {
+    var data = await apiService.get(endPoint: 'volumes?Filtering=free-ebooks&q=programming');
+    List<BookEntity> books = getBooksList(data);
+    return books;
   }
 
   @override
@@ -19,6 +26,12 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource{
     throw UnimplementedError();
   }
 
-
+  List<BookEntity> getBooksList(Map<String, dynamic> data) {
+    List<BookEntity> books = [];
+    for (var bookMap in data['items']) {
+      books.add(BookModel.fromJson(bookMap));
+    }
+    return books;
+  }
 
 }
