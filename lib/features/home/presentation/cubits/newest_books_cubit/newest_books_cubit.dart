77d7c36793bdfcb5ffe754b598ff79333258e539
ARasheed21 <ara4eed21@gmail.com>
2024-07-21 17:08:11 +0300

    create newest books states

---
+++ lib/features/home/presentation/cubits/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,10 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+import '../../../domain/entities/book_entity.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit() : super(NewestBooksInitial());
+}
