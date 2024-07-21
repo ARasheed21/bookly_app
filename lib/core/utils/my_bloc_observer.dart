c0c585a8351bd030708e9be29ad771f59a0b7a02
ARasheed21 <ara4eed21@gmail.com>
2024-07-21 19:06:33 +0300

    some modifications

--- lib/core/utils/my_bloc_observer.dart
+++ lib/core/utils/my_bloc_observer.dart
@@ -5,7 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class MyBlocObserver extends BlocObserver{
   @override
   void onChange(BlocBase bloc, Change change) {
-    debugPrint('chnage: ${change.toString()}');
+    debugPrint('{change.toString()}');
     super.onChange(bloc, change);
   }
 }
