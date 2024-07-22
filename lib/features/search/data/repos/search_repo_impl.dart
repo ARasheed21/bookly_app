2098fd167148821479d3243de970a33503a92a36
ARasheed21 <ara4eed21@gmail.com>
2024-07-23 01:57:15 +0300

    search repo impl

--- lib/features/search/data/repos/search_repo_impl.dart
+++ lib/features/search/data/repos/search_repo_impl.dart
@@ -2,7 +2,6 @@ import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:udemy8_bookly/core/errors/failure.dart';
 import 'package:udemy8_bookly/features/search/domain/entities/book_entity.dart';
-
 import '../../domain/repos/search_repo.dart';
 import '../data_sources/search_remote_data_source.dart';
 
