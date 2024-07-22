41bc9b537087c0d1a0a8cf595b3cee28bf82bd0b
ARasheed21 <ara4eed21@gmail.com>
2024-07-23 02:05:21 +0300

    implement search cubit

--- lib/features/search/presentation/cubits/search_cubit/search_cubit.dart
+++ lib/features/search/presentation/cubits/search_cubit/search_cubit.dart
@@ -1,10 +1,25 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
-import '../../../../home/domain/entities/book_entity.dart';
+import '../../../../search/domain/entities/book_entity.dart';
+import '../../../domain/repos/search_repo.dart';
 
 part 'search_state.dart';
 
 class SearchCubit extends Cubit<SearchState> {
-  SearchCubit() : super(SearchInitial());
+  final SearchRepo searchRepo;
+  SearchCubit(this.searchRepo) : super(SearchInitial());
+
+  Future<void> fetchSearchedBooks({required String bookTitle})async{
+
+    emit(SearchLoading());
+
+    var result = await searchRepo.fetchSearchedBooks(bookTitle: bookTitle);
+
+    result.fold((failure) {
+      emit(SearchFailure(failure.message));
+    }, (books){
+      emit(SearchSuccess(books));
+    });
+  }
 }
